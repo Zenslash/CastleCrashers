60d109eb212133ae23f68fc2188126cce2fd2e90
Fewmade <iskakovartem2001@gmail.com>
2019-03-06 20:04:44 +0300

    Units spawn

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+public enum UNIT_TYPE { KNIGHT, ARCHER};
+
 public class Castle : MonoBehaviour
 {
 
     #region Private Variables
 
+    [SerializeField]
+    private Transform _unitSpawnPoint;
 
+    [SerializeField]
+    private GameObject[] _unitsPrefabs;
+
+
+    private int totalUnitsPrefabs;
 
     #endregion
 
@@ -20,7 +29,49 @@ public class Castle : MonoBehaviour
 
     #region Callbacks
 
+    private void Start()
+    {
+        Init();
 
+        //For development purpose
+        spawnUnit(UNIT_TYPE.KNIGHT);
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    private void Init()
+    {
+        totalUnitsPrefabs = _unitsPrefabs.Length;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    private void spawnUnit(UNIT_TYPE unit_type)
+    {
+
+        GameObject npc;
+        switch(unit_type)
+        {
+            case UNIT_TYPE.KNIGHT:
+
+                npc = Instantiate(_unitsPrefabs[0], _unitSpawnPoint);
+                //Init stuff
+
+                break;
+            case UNIT_TYPE.ARCHER:
+
+                npc = Instantiate(_unitsPrefabs[1], _unitSpawnPoint);
+                //Init stuff
+
+                break;
+        }
+
+    }
 
     #endregion
 
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Goal - based AI architecture
+
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region Private Variables
+
+
+
+    #endregion
+
+    #region Public Variables
+
+
+
+    #endregion
+
+    #region Callbacks
+
+
+
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+
+    #region Public Methods
+
+
+
+    #endregion
+
+
 }
